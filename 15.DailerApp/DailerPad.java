7df0668c1175fab4b05c37b88808c856d9e1ce9d
Eswar aleti <eswaraleti143@gmail.com>
2019-07-06 16:22:03 +0530

    Preserving responsibilty of DailerPad class

--- 15.DailerApp/DailerPad.java
+++ 15.DailerApp/DailerPad.java
@@ -1,53 +1,29 @@
 import java.util.ArrayList;
+import java.util.HashMap; 
+import java.util.Map; 
 public class DailerPad
 {
     public Contact searchContact(ArrayList<Contact> contactList,String num)
 	{
-		String[] numpad={"","ABC","DEF","DEF","JKL","MNO","PQRS","TUV","WXYZ"};
 		for(int i=0;i<contactList.size();i++)
 		{
-			if(contactList.get(i).getFirstHalfPhoneNo().equals(num) || contactList.get(i).getSecondHalfPhoneNo().equals(num))
+			if(contactList.get(i).isNumberMatch(num) || contactList.get(i).isNameMatch(num))
+			{
 				return contactList.get(i);
-		}
-		for(int i=0;i<contactList.size();i++)
-		{
-			if(isMatched(contactList.get(i).getFirstName(),num))
-				return contactList.get(i);
-			if(isMatched(contactList.get(i).getLastName(),num))
-				return contactList.get(i);
+			}
 		}
 		return null;
 	}
-	private boolean isMatched(String name,String num)
+	public static char getNumFromNumpad(char ch)
 	{
-		if(name=="" || name.length()!=num.length())
-			return false;
-		name=name.toUpperCase();
-		for(int i=0;i<name.length();i++)
+		String[][] numPad={{"ABC","2"},{"DEF","3"},{"GHI","4"},{"JKL","5"},{"MNO","6"},{"PQRS","7"},{"TUV","8"},{"WXYZ","9"}};
+		for(int i=0;i<numPad.length;i++)
 		{
-			if(getNumFromNumpad(name.charAt(i))!=num.charAt(i))
-				return false;
+			if (numPad[i][0].contains(ch+"")) 
+			{
+				return numPad[i][1].charAt(0);
+			}
 		}
-		return true;
-	}
-	private char getNumFromNumpad(char ch)
-	{
-		if("ABC".contains(ch+""))
-			return '2';
-		if("DEF".contains(ch+""))
-			return '3';
-		if("GHI".contains(ch+""))
-			return '4';
-		if("JKL".contains(ch+""))
-			return '5';
-		if("MNO".contains(ch+""))
-			return '6';
-		if("PQRS".contains(ch+""))
-			return '7';
-		if("TUV".contains(ch+""))
-			return '8';
-		if("WXYZ".contains(ch+""))
-			return '9';
 		return '0';
 	}
 }
